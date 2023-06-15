4ac112a0e913cc735bd5ad26ed0e78a32c54d2c9
Viktor Maltsev <viktormaltsev@Viktors-Mac-mini.local>
2023-06-15 13:45:18 +0500

    2 packages

--- Package.swift
+++ Package.swift
@@ -7,12 +7,18 @@ let package = Package(
     name: "Affise",
     products: [
         .library(name: "AffiseAttributionLib", targets: ["AffiseAttributionLib"]),
+        .library(name: "AffiseSKAdNetwork", targets: ["AffiseSKAdNetwork"]),
     ],
     targets: [
         .target(
             name: "AffiseAttributionLib",
             dependencies: [],
             path: "AffiseAttributionLib"
+        ),
+        .target(
+            name: "AffiseSKAdNetwork",
+            dependencies: [],
+            path: "AffiseSKAdNetwork"
         ),
     ]
 )
