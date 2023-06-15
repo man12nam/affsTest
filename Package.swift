9daff77e0af67364d7b12a27be0ff7cbff97941a
Viktor Maltsev <viktormaltsev@Viktors-Mac-mini.local>
2023-06-15 14:27:06 +0500

    package fix

--- Package.swift
+++ Package.swift
@@ -7,19 +7,12 @@ let package = Package(
     name: "Affise",
     products: [
         .library(name: "AffiseAttributionLib", targets: ["AffiseAttributionLib"]),
-        .library(name: "AffiseSKAdNetwork", targets: ["AffiseSKAdNetwork"]),
     ],
     targets: [
         .target(
             name: "AffiseAttributionLib",
             dependencies: [],
             path: "AffiseAttributionLib",
-            publicHeadersPath:"include"
-        ),
-        .target(
-            name: "AffiseSKAdNetwork",
-            dependencies: [],
-            path: "AffiseSKAdNetwork",
             publicHeadersPath:"include"
         ),
     ]
